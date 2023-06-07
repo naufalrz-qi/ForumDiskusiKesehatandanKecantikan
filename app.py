eb84a013a6dc411fc13396dddd799909c4f57f01
Naufal Rifqi Zuhrian <estarossa.nrz@gmail.com>
2023-06-07 12:25:37 +0800

    Penambahan base sebagai template navbar, gambar pada register, dan routenya

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, jsonify
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)

144fd874c5a1d426d89c35a2ffeaf3aed9223976
Naufal Rifqi Zuhrian <estarossa.nrz@gmail.com>
2023-06-08 14:26:37 +0800

    Fitur register telah diselesaikan

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template, redirect, url_for, jsonify
+from flask import Flask, render_template, redirect, url_for, jsonify, request
+from pymongo import MongoClient
+import jwt
+from datetime import datetime, timedelta
+import hashlib
+from werkzeug.utils import secure_filename
+
+
 app = Flask(__name__)
 
+app.config['TEMPLATES_AUTO_RELOAD']=True
+app.config['UPLOAD_FOLDER'] = './static/profile_pics'
+
+SECRET_KEY = 'TimProjek2'
+MONGODB_CONNECTION_STRING = 'mongodb+srv://user1:123@cluster0.g1iutgc.mongodb.net/?retryWrites=true&w=majority'
+client = MongoClient(MONGODB_CONNECTION_STRING)
+db = client.finalprojectForumHnB
+TOKEN_KEY = 'mytoken'
 
 @app.route('/')
 def index():
@@ -9,6 +24,54 @@ def index():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form["username_give"]
+    email_receive = request.form["email_give"]
+    password_receive = request.form["password_give"]
+    role_receive = request.form["role_give"]
+    password_hash = hashlib.sha256(password_receive.encode("utf-8")).hexdigest()
+    if(role_receive == 'expert'):
+        doc = {
+            "username": username_receive,                               
+            "password": password_hash,                                  
+            "profile_name": username_receive,
+            "email":email_receive,
+            "role":role_receive,                           
+            "gender":"",
+            "academic_info":"",
+            "workplace":"",
+            "service":"",
+            "phone_number":"",
+            "profile_pic": "",                                          
+            "profile_pic_real": "profile_pics/profile_placeholder.png", 
+            "profile_info": ""                                            
+            }
+        db.expert_users.insert_one(doc)
+        return jsonify({'result': 'success'})
+    elif(role_receive == 'normal'):
+        doc = {
+            "username": username_receive,                               
+            "password": password_hash,     
+            "email":email_receive,
+            "role":role_receive,                                 
+            "profile_name": username_receive,                           
+            "profile_pic": "",                                          
+            "profile_pic_real": "profile_pics/profile_placeholder.png", 
+            "profile_info": ""                                          
+        }
+        db.normal_users.insert_one(doc)
+        return jsonify({'result': 'success'})
+    else:
+        return jsonify({'result': 'failed'})
+
+@app.route("/sign_up/check_dup", methods=["POST"])
+def check_dup():
+    username_receive = request.form.get('username_give')
+    exists = bool(db.normal_users.find_one({'username':username_receive}))
+    exists2 = bool(db.expert_users.find_one({'username':username_receive}))
+    return jsonify({"result": "success", 'exists':exists+exists2})
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
---
+++ static/register.js
@@ -0,0 +1,147 @@
+
+document.getElementById("input_user").onchange = function() {check_dup()};
+
+function sign_up() {
+    let helpUser = $('#help_user')
+    let inputPassword = $('#input_pass')
+    let inputPassword2 = $('#input_pass2')
+    let inputEmail = $('#input_email')
+    let selectRole = $('#role_select')
+
+
+    let username = $('#input_user').val();
+    let email = inputEmail.val();
+    let password = inputPassword.val();
+    let password2 = inputPassword2.val();
+    let role = selectRole.val()
+    
+    if (helpUser.hasClass('is-danger')){
+        alert('Please check your id')
+        return;
+    }else if(!helpUser.hasClass('is-success')){
+        alert('Please double check your id, something is wrong')
+        return;
+    }
+
+    let helpPassword = $('#help_pass')
+    let helpPassword2 = $('#help_pass2')
+    let helpRole = $('#help_role')
+
+    
+
+    if (password === ''){
+        helpPassword.text('Please enter your password')
+                    .removeClass('is-safe')
+                    .addClass('is-danger')
+        inputPassword.focus()
+        return;
+    }else if (!is_password(password)){        
+        helpPassword.text('For your password, please enter 8-20 English characters, numbers, or the following special characters (!@#$%^&*)')
+                    .removeClass('is-safe')
+                    .addClass('is-danger')
+        inputPassword.focus()
+        return;
+    }else{
+        helpPassword.text('This password can be used!')
+                    .removeClass('is-danger')
+                    .addClass('is-success')
+    }
+
+
+    if (password2 === ''){
+        helpPassword2.text('Please enter your password')
+                    .removeClass('is-safe')
+                    .addClass('is-danger')
+        inputPassword2.focus()
+        return;
+    } else if(password2 != password){
+        helpPassword2.text("Password doesn't match")
+                    .removeClass('is-safe')
+                    .addClass('is-danger')
+        inputPassword2.focus()
+        return;
+    }
+
+    if(role === ''){
+        helpRole.text('Please select one of them between normal user or an expert')
+        .removeClass('is-safe')
+        .addClass('is-danger')
+        selectRole.focus()
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/sign_up/save",
+        data: {
+        username_give: username,
+        email_give: email,
+        password_give: password,
+        role_give: role,
+
+        },
+        success: function (response) {
+        if(response["result"] == 'success'){
+            alert("You are registered! Nice!");
+            window.location.replace("/login");
+        }else{
+            alert("Something went wrong")
+        }
+        
+        },
+    });
+    }
+
+function is_username(asValue) {
+    var regExp = /^(?=.*[a-zA-Z])[-a-zA-Z0-9_.]{5,15}$/;
+    return regExp.test(asValue);
+}
+
+function is_password(asValue) {
+    var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,20}$/;
+    return regExp.test(asValue);
+}
+
+function check_dup(){
+    let inputUsername = $('#input_user')
+    let helpUser = $('#help_user')
+    let username = inputUsername.val()
+    if (username === ''){
+        helpUser.text("Please enter your username")
+                .removeClass('is-safe')
+                .addClass('is-danger')
+        inputUsername.focus();
+        return;
+    }
+
+    if (!is_username(username)){
+        helpUser.text("For your username, please type in 5-15 English characters, numbers, or special characters(._-)")
+                .removeClass('is-safe')
+                .addClass('is-danger');
+        inputUsername.focus();
+        return;
+    }
+
+    helpUser.addClass('is-loading');
+
+    $.ajax({
+        type:'POST',
+        url:'/sign_up/check_dup',
+        data:{
+            username_give:username,
+        },
+        success: function(response){
+            if (response['exists']){
+                helpUser.text('This is already in use')
+                        .removeClass('is-safe')
+                        .addClass('is-danger')
+                inputUsername.focus()
+            }else{
+                helpUser.text('This username is available for use!')
+                        .removeClass('is-danger')
+                        .addClass('is-success')
+            }
+            helpUser.removeClass('is-loading')
+        }
+    })
+}
